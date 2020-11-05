a00b666bbef77816534b6d0484e6643de82486ac
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-11-04 18:15:24 -0600

    cleaned up index.html and default react files

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -3,7 +3,11 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom'
 
 class Table extends Component {
-
+  
+  componentDidMount() {
+    console.log('table mounted');
+  }
+  
   render() {
     return (
       <div className='root'>
